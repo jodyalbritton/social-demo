4650251882611465adaa6471eeeae27264b4670c
jodyalbritton <jody.albritton@gmail.com>
2012-09-28 18:52:45 -0500

    more redis fix

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,17 @@
+ class LikesController < ApplicationController
+
+    def create
+      target = Likeable.find_by_resource_id(params[:resource_name], params[:resource_id])
+      current_user.like!(target)
+      @like_object = target.activities.first
+
+     
+    end
+
+    def destroy
+      target = Likeable.find_by_resource_id(params[:resource_name], params[:resource_id])      
+      current_user.unlike!(target)
+      @like_object = target.activities.first
+
+    end
+  end
