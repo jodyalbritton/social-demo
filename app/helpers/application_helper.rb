6fd86d25734f66d06505c72f204bc5da59c12027
jodyalbritton <jody.albritton@gmail.com>
2012-09-29 18:11:14 -0500

    fixed linking of usernames and hashtags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,22 +12,22 @@ module ApplicationHelper
   end
   
   def mention_linker(content)
-    content.gsub(/\B@(\w*[A-Za-z0-9_]+\w*)/) do
-    linker = User.where(:username => $1)
-     if linker.any?
-      link_to $1, user_path($1)
-     else 
-       $1
-     end 
-    end
-    content.gsub(/\B#(\w*[A-Za-z0-9_]+\w*)/) do
-    linker = Tag.where(:content => $1)
-     if linker.any?
-      link_to $1, tag_path(linker.last.id)
-     else 
-       $1
-     end 
-    end
+    content.gsub(/\B*([@#])(\w*[A-Za-z0-9_]+\w*)/) do
+    if $1 == "@"
+      linker = User.where(:username => $2)
+        if linker.any?
+        link_to $2, user_path(linker.last.username) 
+        else 
+        $2
+        end  
+   elsif $1 == "#"
+     linker = Tag.where(:content => $2)
+       if linker.any?
+       link_to $2, tag_path(linker.last.id) 
+       else 
+       $2
+       end
+   end 
   end
-  
+  end
 end
