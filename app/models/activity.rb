b163d691548463766bd8f31d0bee78ebdd742d5d
jodyalbritton <jody.albritton@gmail.com>
2012-10-10 06:31:43 -0500

    x

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -8,26 +8,23 @@ class Activity < ActiveRecord::Base
   belongs_to :target, :polymorphic => true
   has_many :replies, :class_name => "Post", :foreign_key => "reply_to_id"
   has_and_belongs_to_many :tags
-  
+  serialize :tag_list
+  after_commit :process_tags
   #default scope and set per page limit for will_paginate
   default_scope :order => 'activities.created_at DESC'
   self.per_page = 10
- 
+  
   #set the partial path for the observer
   def to_partial_path() 
     "activities/#{target_type.downcase}" 
   end
 
   #Flatten, generate and process tags.
-   serialize :tag_list
-   before_save :generate_taglist
-   after_commit :process_tags
+ 
+   
   
   private
-  
-  def generate_taglist
-     self.tag_list = self.target.content.scan(/\B#(\w*[A-Za-z0-9_]+\w*)/).flatten
-  end
+
   def process_tags
     TAG_PROCESSOR.push(:activity_id => self.id)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 require 'file_size_validator'
 class Post < ActiveRecord::Base
    
-   attr_accessible :content, :user, :parent_id, :scope, :date, :attachment, :reply_to_id
+   attr_accessible :content, :user, :parent_id, :scope, :date, :attachment, :reply_to_id, :tag_list
    
    #includes 
    include Likeable
@@ -13,12 +13,15 @@ class Post < ActiveRecord::Base
    belongs_to :user
    
    has_many :activities, :as => :target, dependent: :destroy
-  
+   serialize :tag_list
+   before_save :generate_taglist
    
    #validations 
    
    validates_presence_of :content
    
-    
-
+  private 
+  def generate_taglist
+     self.tag_list = self.content.scan(/\B#(\w*[A-Za-z0-9_]+\w*)/).flatten
+  end
 end
